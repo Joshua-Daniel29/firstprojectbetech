f72efc7d9045c0a21ebff50374d63a5ac5fb8c5b
Joshua-Mondie <joshuamondie@gmail.com>
2024-03-06 11:47:04 +0100

    changes

---
+++ src/redux/reducers/index.tsx
@@ -0,0 +1,10 @@
+// // reducers/index.ts
+// import { combineReducers } from 'redux';
+// import { reducer as someReducer } from './authReducer';
+
+// const rootReducer = combineReducers({
+//   some: someReducer,
+//   // add other reducers here if needed
+// });
+
+// export default rootReducer;
