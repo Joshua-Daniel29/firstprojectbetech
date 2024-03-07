ed484c87edae13f80fd7af8610d9bc3b1e542bc4
Joshua-Daniel29 <joshuamondie@betech.ng>
2024-03-07 17:34:19 +0100

    changes

--- src/api/ApiAuth.tsx
+++ src/api/ApiAuth.tsx
@@ -3,13 +3,14 @@ import { isConstructorDeclaration } from "typescript";
 
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 
-console.log('url',baseURL);
+console.log("url", baseURL);
 
 const ApiAuth = axios.create({
-    baseURL,
-    headers: {
-        Accept: "application/json"
-    }
-})
+  baseURL,
+  headers: {
+    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+    Accept: "application/x-www-form-urlencoded",
+  },
+});
 
 export default ApiAuth;
