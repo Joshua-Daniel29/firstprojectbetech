1dcb07958e701d97ff7312f057cd5d74367d5be7
Joshua-Daniel29 <joshuamondie@betech.ng>
2024-03-12 15:39:26 +0100

    changes

--- src/redux/features/authslice.tsx
+++ src/redux/features/authslice.tsx
@@ -7,7 +7,6 @@ export const loginUser = createAsyncThunk(
   async (credentials: any) => {
     const request = await ApiAuth.post("/login", credentials);
     const response = await request.data;
-    // console.log('login-response', response);
     return response;
   }
 )
